6124ef7a16fdbdb23b13b4c1bec27e788f7b3961
Fennec33 <66853066+Fennec33@users.noreply.github.com>
2022-07-10 22:41:30 -0700

    Initial

---
+++ Galactic Throne/GalacticThrone/GameManager.cs
@@ -0,0 +1,20 @@
+﻿namespace GalacticThrone;
+using System;
+
+public static class GameManager
+{
+    private static void Main()
+    {
+        Console.WriteLine("Start");
+        
+        while (true)
+        {
+            string? input = Console.ReadLine();
+
+            if (input!.Equals("end", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Galactic Throne/GalacticThroneTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace GalacticThroneTests;
+using GalacticThrone;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
